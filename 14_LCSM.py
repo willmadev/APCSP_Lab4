3fbc500b5ded7de5892d896abbd9374e4497745a
willmas-here <willthemeh@gmail.com>
2020-12-16 22:34:02 +0800

    Finished LCSM

---
+++ 14_LCSM.py
@@ -0,0 +1,34 @@
+## Problem 14 - LCSM (Finding a Shared Motif)
+## Willma
+## 12/16/2020
+
+## http://rosalind.info/problems/lcsm/
+
+import resources
+
+file_path = 'datasets/rosalind_lcsm.txt'
+dna_list = [data for data in resources.read_fasta(file_path).values()]
+
+longest_substring = ''
+start_loc = 0
+nt = 0
+while nt < len(dna_list[0]):
+    end_loc = nt
+    substring_works = True
+    while substring_works == True:
+        substring = dna_list[0][nt:end_loc]
+
+        for dna in dna_list:
+            #if substring is not in dna
+            if dna.find(substring) == -1:
+                substring_works = False
+        
+        #if substring is in all dna
+        if substring_works == True:
+            if len(substring) > len(longest_substring):
+                longest_substring = substring
+            end_loc += 1
+            if end_loc > len(dna_list[0]):
+                substring_works = False
+    nt += 1
+print(longest_substring)
