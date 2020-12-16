6f90c0e92a77499ec2b3d8f271a90c01e153dd42
willmas-here <willthemeh@gmail.com>
2020-12-16 17:47:18 +0800

    Finished PROT, added codon dict to resources

---
+++ 8_PROT.py
@@ -0,0 +1,19 @@
+## Problem 7 - PROT (Translating RNA into Protein)
+## Willma
+## 12/16/2020
+
+## http://rosalind.info/problems/prot/
+
+import resources
+
+file_path = 'datasets/rosalind_prot.txt'
+rna = resources.read_file(file_path).strip()
+
+i=0
+prot = ''
+while i < len(rna):
+    window = rna[i:i+3]
+    prot += resources.codonDict[window]
+    i+=3
+
+print(prot.strip('*'))
--- resources.py
+++ resources.py
@@ -35,3 +35,24 @@ def read_fasta(file_path):
             dictionary[key] = sequence
     
     return(dictionary)
+
+codonDict = {
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L', 'UUA': 'L', 'UUG': 'L',
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V', 'UUU': 'F', 'UUC': 'F', 'AUG': 'M', 'UGU': 'C', 'UGC': 'C',
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A', 'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G', 'CCU': 'P',
+    'CCC': 'P', 'CCA': 'P', 'CCG': 'P', 'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T', 'UCU': 'S', 'UCC': 'S',
+    'UCA': 'S', 'UCG': 'S', 'AGU': 'S', 'AGC': 'S', 'UAU': 'Y', 'UAC': 'Y', 'UGG': 'W', 'CAA': 'Q', 'CAG': 'Q',
+    'AAU': 'N', 'AAC': 'N', 'CAU': 'H', 'CAC': 'H', 'GAA': 'E', 'GAG': 'E', 'GAU': 'D', 'GAC': 'D', 'AAA': 'K',
+    'AAG': 'K', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 'AGA': 'R', 'AGG': 'R', 'UAA': '*', 'UAG': '*',
+    'UGA': '*'
+}
+
+aaDict = {
+    'A': ['GCU', 'GCC', 'GCA', 'GCG'], 'C': ['UGU', 'UGC'], 'D': ['GAU', 'GAC'], 'E': ['GAA', 'GAG'], 
+    'F': ['UUU', 'UUC'], 'G': ['GGU', 'GGC', 'GGA', 'GGG'], 'H': ['CAU', 'CAC'], 'I': ['AUU', 'AUC', 'AUA'], 
+    'K': ['AAA', 'AAG'], 'L': ['CUU', 'CUC', 'CUA', 'CUG', 'UUA', 'UUG'], 'M': ['AUG'], 'N': ['AAU', 'AAC'], 
+    'P': ['CCU', 'CCC', 'CCA', 'CCG'], 'Q': ['CAA', 'CAG'], 'R': ['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'], 
+    'S': ['UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'], '*': ['UAA', 'UAG', 'UGA'], 'T': ['ACU', 'ACC', 'ACA', 'ACG'], 
+    'V': ['GUU', 'GUC', 'GUA', 'GUG'], 'W': ['UGG'], 'Y': ['UAU', 'UAC']
+}
+
